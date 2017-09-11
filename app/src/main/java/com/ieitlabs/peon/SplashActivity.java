f009216c846f43bd7d6bde254612d39e1b44ae07
Masud Rahman <masud7866@users.noreply.github.com>
2017-09-11 23:39:01 +0600

    With working SplashScreen

--- app/src/main/java/com/ieitlabs/peon/SplashActivity.java
+++ app/src/main/java/com/ieitlabs/peon/SplashActivity.java
@@ -3,13 +3,35 @@ package com.ieitlabs.peon;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+               setContentView(R.layout.activity_splash);
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,SideBar.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
