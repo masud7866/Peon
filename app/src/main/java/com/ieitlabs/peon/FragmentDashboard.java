aeb74c62897453d2cc2d55256ecfc4eed35a3b83
Arifuzzaman Pranto <arifuzzamanpranto@ymail.com>
2017-09-20 19:43:59 +0600

    Manage User Added

--- app/src/main/java/com/ieitlabs/peon/FragmentDashboard.java
+++ app/src/main/java/com/ieitlabs/peon/FragmentDashboard.java
@@ -64,7 +64,11 @@ public class FragmentDashboard extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_dashboard, container, false);
+        View v = inflater.inflate(R.layout.fragment_fragment_dashboard, container, false);
+
+
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
